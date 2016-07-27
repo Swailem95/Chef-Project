06dc9ecf65195852e3bdf545c9620c33bb9e20aa
Youssef <youssefswailem@gmail.com>
2016-07-28 01:09:53 +0600

    Update default.rb

--- open-source-cookbooks/java/attributes/default.rb
+++ open-source-cookbooks/java/attributes/default.rb
@@ -21,8 +21,8 @@
 default['java']['jdk_version'] = '6'
 default['java']['arch'] = kernel['machine'] =~ /x86_64/ ? 'x86_64' : 'i586'
 default['java']['openjdk_packages'] = []
-default['java']['openjdk_version'] = nil
-default['java']['accept_license_agreement'] = false
+default['java']['openjdk_version'] = '6'
+default['java']['accept_license_agreement'] = true
 default['java']['set_default'] = true
 default['java']['alternatives_priority'] = 1062
 default['java']['set_etc_environment'] = false
@@ -80,7 +80,7 @@ when 'oracle_rpm'
 end
 
 # if you change this to true, you can download directly from Oracle
-default['java']['oracle']['accept_oracle_download_terms'] = false
+default['java']['oracle']['accept_oracle_download_terms'] = true
 
 # direct download paths for oracle, you have been warned!
 
