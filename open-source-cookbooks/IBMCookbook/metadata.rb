afe9a7b1b64d4654d1bb4d0eb5b797e868ed2156
Youssef Swailem <youssefswailem@gmail.com>
2016-07-28 14:02:38 +0200

    Added IBMCookbook which runs the other ones

---
+++ open-source-cookbooks/IBMCookbook/metadata.rb
@@ -0,0 +1,11 @@
+name 'IBMCookbook'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs and configures various applications and services'
+long_description 'Installs and Configures Tomcat, IIS, WLP, DB2 and WAS Network Deployment'
+version '0.1.0'
+
+depends 'tomcat'
+depends 'iis'
+depends 'wlp'
