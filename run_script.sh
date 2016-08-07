b83143a109804f6e65ea5947bed03b960a5f114e
Youssef Swailem <youssefswailem@gmail.com>
2016-08-07 11:27:48 +0200

    Updated run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -8,5 +8,10 @@ path="$(pwd)/open-source-cookbooks"
 #Create the solo.rb file with the path inside it
 echo "cookbook_path ['$path']" > $path/solo.rb
 
-#execute the IBMCookbook which runs the other cookbooks
-sudo chef-solo -c $path/solo.rb -o recipe[IBMCookbook]
+if [ $# -eq 0 ]
+    then
+        #execute the IBMCookbook which runs the other cookbooks
+        sudo chef-solo -c $path/solo.rb -o recipe[IBMCookbook]
+else
+    sudo chef-solo -c $path/solo.rb -o recipe[$1]
+fi
