1a9a6d3eb4695de7257ed0e93ce3c97f271add43
Youssef Swailem <youssefswailem@gmail.com>
2016-08-08 10:43:03 +0200

    Updated run_script.sh

--- run_script.sh
+++ run_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-###execute this script to run the IBM cookbook
+###execute this script to run the IBM cookbook or any other cookbook.
 
 #Get the relative path of the cookbooks
 path="$(pwd)/open-source-cookbooks"
@@ -9,9 +9,10 @@ path="$(pwd)/open-source-cookbooks"
 echo "cookbook_path ['$path']" > $path/solo.rb
 
 if [ $# -eq 0 ]
-    then
-        #execute the IBMCookbook which runs the other cookbooks
-        sudo chef-solo -c $path/solo.rb -o recipe[IBMCookbook]
+then
+    #execute the IBMCookbook which runs the other cookbooks
+    sudo chef-solo -c $path/solo.rb -o recipe[IBMCookbook]
 else
+    ##or pass another cookbook to the script to run it instead
     sudo chef-solo -c $path/solo.rb -o recipe[$1]
 fi
